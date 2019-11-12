fd20a84000418bbb0656d716906bda1ccd799ef2
rogerio0089 <rogeriosandrojunior@hotmail.com>
2019-11-12 13:08:23 +0000

    Modification in hello.fragment

--- webapp/view/hello.fragment.js
+++ webapp/view/hello.fragment.js
@@ -1,8 +1,22 @@
-sap.ui.jsfragment( "testes2.Testes2.view.hello",{ 
-	createContent: function (oController ) {
-        var oButton  = new sap.m.Button({ 
-			text: "Hello World"
-		}); 
-		return oButton; 
-	} 
+sap.ui.define(["sap/m/Button"], function (Button) {
+	sap.ui.jsfragment("testes2.Testes2.view.hello", {
+		createContent: function (oController) {
+			var oFooter = new sap.m.Bar({
+				contentLeft: [
+					new Button({
+						text: "Editar Venda",
+						type: "Up"
+					})
+				],
+				contentRight: [
+					new Button({
+						text: "Salvar Venda",
+						type: "Accept",
+						press: [oController.onApproveDialog]
+					})
+				]
+			});
+			return oFooter;
+		}
+	});
 });
